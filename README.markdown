5c2b96fcc275b911010e8f0454f2d8dd72719a0c
Clarissa Colaço Ramos <clarissa.colaco@hotmail.com>
2021-04-04 01:52:44 -0300

    primeiro commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Mentalista
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/clarcolaco/pen/GRroRor](https://codepen.io/clarcolaco/pen/GRroRor).
+
+projeto imersão dev 2021
---
+++ src/script.js
@@ -0,0 +1,74 @@
+  function sorteia() {
+    
+           return Math.round(Math.random() * 10);
+    
+        }
+    
+        function sorteiaNumeros(quantidade) {
+    
+            var segredos = [];
+    
+            var numero = 1;
+    
+            while(numero <= quantidade) {
+    
+                  var numeroAleatorio = sorteia();
+                  var achou = false;
+    
+                  if (numeroAleatorio !== 0) {
+                         for(var posicao = 0; posicao < segredos.length; posicao++) {
+    
+                               if(segredos[posicao] == numeroAleatorio){
+                                    achou = true;
+                                    break;
+                               }
+    
+                         }
+    
+                         if (achou == false) {
+                               segredos.push(numeroAleatorio);
+                               numero++;
+                         }
+                  }
+    
+            }
+    
+            return segredos;
+    
+        }
+    
+       
+
+
+        var segredos = sorteiaNumeros(1);
+        console.log(segredos);
+        var input = document.querySelector("input");
+        input.focus();
+    
+        function verifica() {
+    
+           var achou = false;
+    
+           for(var posicao = 0; posicao < segredos.length; posicao++) {
+    
+                  if(input.value == segredos[posicao]) {
+    
+                         alert("Você ACERTOU! O número era " + segredos);
+                         achou = true;
+                         break;
+                  } 
+           }
+    
+           if(achou == false) {
+    
+                  alert("Você ERROU! o nunero era " + segredos);
+           }
+    
+           input.value = "";
+           input.focus();
+    
+        }
+    
+        var button = document.querySelector("button");
+    
+        button.onclick = verifica;
